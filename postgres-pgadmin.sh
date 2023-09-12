93b3105a3b775698257fcd61a5c5c14cf2865f58
Rahadyan Rizqy <rahadyanrizqy@gmail.com>
2023-09-12 09:28:18 +0700

    Update postgres-pgadmin.sh

--- postgres-pgadmin.sh
+++ postgres-pgadmin.sh
@@ -1,48 +1,77 @@
 #!/bin/bash
 #Tested on Ubuntu 22.04
+#Tested on Debian 11
+#Last test September 12th, 2023
 
-apt update
+install() {
+    apt update
+    apt install postgresql postgresql-contrib -y
+    service postgresql restart
 
-apt install postgresql postgresql-contrib -y
+    echo "Create user for PostgreSQL"
+    apt install lsb-release -y
+    read -p "Username:" username
 
-service postgresql restart
+    if [ $(lsb_release -is) == 'Debian' ]; then
+        su - postgres -c "createuser --superuser $username"
+    else
+        su -u postgres createuser --superuser $username
+    fi
 
-echo "Create user for PostgreSQL"
-read -p "Username:" username
+    adduser $username
 
-su -u postgres createuser --superuser $username
+    apt install curl -y
+    apt install gnupg -y
 
-adduser $username
+    curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | apt-key add
 
-apt install curl -y
+    sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list &&  apt update'
 
-apt install gnupg -y
+    apt install pgadmin4-web -y
 
-apt install lsb-release -y
+    /usr/pgadmin4/bin/setup-web.sh
 
-curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | apt-key add
+    echo "host    all             all             192.168.1.0/24          trust" >> /etc/postgresql/14/main/pg_hba.conf
 
-sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list &&  apt update'
+    sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '0.0.0.0'/" /etc/postgresql/14/main/postgresql.conf
 
-apt install pgadmin4-web -y
+    apt install iptables-persistent -y
 
-/usr/pgadmin4/bin/setup-web.sh
+    iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
+    iptables -A OUTPUT -p tcp --dport 3306 -j ACCEPT
+    iptables-save > /etc/iptables/rules.v4
 
-echo "host    all             all             192.168.1.0/24          trust" >> /etc/postgresql/14/main/pg_hba.conf
+    service iptables restart
+    service postgresql restart
 
-sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '0.0.0.0'/" /etc/postgresql/14/main/postgresql.conf
+    ipv4=$(ip a | grep -oP 'inet \K[\d.]+' | tail -1)
+    echo "Your PostgreSQL now can be accessed remotely on $ipv4:5432 and for pgAdmin4 http://$ipv4/pgadmin4/"
+}
 
-apt install iptables-persistent -y
+uninstall() {
+	apt remove postgresql -y
+	apt remove postgresql-contrib -y
+	apt remove curl -y
+	apt remove gnupg -y
+	apt remove pgadmin4-web -y
+	apt remove iptables-persistent -y
+	apt autoremove -y
+	apt autoclean
+	apt clean
+	rm -rf /var/lib/pgadmin
+	rm -rf /usr/pgadmin4
+    username=$(tail -n 1 /etc/passwd | cut -d: -f1)
+    userdel -r $username
+    userdel $username
+}
 
-iptables -A INPUT -p tcp --dport 3306 -j ACCEPT
+echo "Choose your option: "
+echo "[1]: Install PostgreSQL and pgAdmin4"
+echo "[2]: Uninstall PostgreSQL and pgAdmin4"
+read -p ": " option
 
-iptables -A OUTPUT -p tcp --dport 3306 -j ACCEPT
-
-iptables-save > /etc/iptables/rules.v4
-
-service iptables restart
-
-service postgresql restart
-
-ipv4=$(ip a | grep -oP 'inet \K[\d.]+' | tail -1)
-echo "Your PostgreSQL now can be accessed remotely on $ipv4:5432 and for pgAdmin4 http://$ipv4/pgadmin4/"
+if [ "$option" == '1' ]; then
+    install
+else
+    uninstall
+fi
