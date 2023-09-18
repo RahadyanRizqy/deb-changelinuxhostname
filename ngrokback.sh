fa712b298209452653def11f8a42c6219cfe1c35
Rahadyan Rizqy <rahadyanrizqy@gmail.com>
2023-09-18 23:52:53 +0700

    Create ngrokback.sh

---
+++ ngrokback.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# ngrok must be placed and run in root server
+/root/ngrok tcp 22 --log=stdout > /root/ngrok.log &
+command=$(ps aux | grep ngrok | awk '{print $2}' | sed -n '2p');
+sleep 5;
+host=$(grep "started tunnel" ngrok.log | awk -F'url=' '{print $2}' | cut -d':' -f2 | sed 's/\/\///');
+port=$(grep "started tunnel" ngrok.log | awk -F'url=' '{print $2}' | cut -d':' -f3);
+echo "Ngrok PID: " $command;
+echo "Host: " $host;
+echo "Port: " $port;
