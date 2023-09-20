72fa6305f1e8899dd4a30839b6e0d6890a40a833
Rahadyan Rizqy <rahadyanrizqy@gmail.com>
2023-09-20 22:25:45 +0700

    Update createbulkuser.sh

--- createbulkuser.sh
+++ createbulkuser.sh
@@ -1,17 +1,25 @@
-#!/bin/bash
+COMMON_PASSWORD="pwebajaya"
+DBPASSWORD=$COMMON_PASSWORD
 
-# Define the common password
-COMMON_PASSWORD="your password"
+read -p "Mysql Password: " ROOTPASSWORD
+read -p "File List Input: " txtfile
 
 # Read usernames from the list file and create users
 while IFS= read -r USERNAME; do
   PASSWORD=$(openssl passwd -1 $COMMON_PASSWORD)  # Generate hashed password
+  DBUSER=$USERNAME
 
-  sudo useradd -m -p $PASSWORD $USERNAME  # Create user with home directory
+  sudo useradd -m -p $PASSWORD $USERNAME # Create user with home directory
   sudo chsh -s /bin/bash $USERNAME
   sudo chown -R $USERNAME:$USERNAME /home/$USERNAME
   find /home/$USERNAME -type d -exec chmod 755 {} \;
   find /home/$USERNAME -type f -exec chmod 644 {} \;
   cp -v index.html /home/$USERNAME;
-  echo "User $USERNAME created with password: $COMMON_PASSWORD"
+
+  mysql -u root -p$ROOTPASSWORD -e "CREATE DATABASE $DBUSER;"
+  mysql -u root -p$ROOTPASSWORD -e "CREATE USER '$DBUSER'@'localhost' IDENTIFIED BY '$DBPASSWORD';"
+  mysql -u root -p$ROOTPASSWORD -e "GRANT ALL PRIVILEGES ON $DBUSER.* TO '$DBUSER'@'localhost';"
+  mysql -u root -p$ROOTPASSWORD -e "FLUSH PRIVILEGES;"
+
+  echo "User $USERNAME and its database created with password: $COMMON_PASSWORD";
 done < pwebauser.txt # list of user from txt in same directory
